317eaaa2ddcf4610f95d477cf24d2518f5771b4d
TonioF <tonio.fincke@brockmann-consult.de>
2020-10-29 16:45:33 +0100

    do not add variables to dataset descriptor

--- xcube_cci/dataaccess.py
+++ xcube_cci/dataaccess.py
@@ -148,7 +148,8 @@ class CciOdpDataOpener(DataOpener):
                     var_descriptors.append(VariableDescriptor(var_name, var_dtype, var_dims, var_info))
             else:
                 var_descriptors.append(VariableDescriptor(var_name, '', ''))
-        ds_metadata.pop('variables')
+        if 'variables' in ds_metadata:
+            ds_metadata.pop('variables')
         ds_metadata.pop('dimensions')
         ds_metadata.pop('variable_infos')
         ds_metadata.pop('attributes')
